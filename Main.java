c5c2d391c258b89af436e267361f14197959d35b
Chelitoone <marcelosepulveda777@gmail.com>
2024-10-06 22:43:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
    Banco bancoMuchoDinero = new Banco("Mi Banco");
 
    // Agregar las cuentas al banco
-   bancoMuchoDinero.adicionarCuenta("12345-6", 200000, "Ahorros", "1234567890 ", "Salvatore Mancuso");
+   bancoMuchoDinero.adicionarCuenta("12345-6", 200000, "Ahorros", "1234567890 ", "Jaime Molina");
    bancoMuchoDinero.adicionarCuenta("78901-2", 350000 , "Corriente", "24307665", "Lady Gaga");
 
    System.out.println("Saldo total del banco: " + bancoMuchoDinero.consultarTotalDinero());
